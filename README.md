aed7cf476c416c43ecd6e8bc92572477fd8094d1
Weiyu Chen <weiyuchens@sina.com>
2022-07-28 17:05:49 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Deep Learning-Based Methods for Argumentation Mining in Amazon Review Texts
+> Author: weiyuchen
+
+## Pretrained Model
+
+### 1. Base Model
+```text
+File Name: uncased_L-12_H-768_A-12 
+Description: BERT-Base, Uncased
+Language Type: English
+Network Structure: 12-layer, 768-hidden, 12-heads
+Parameter Scale: 110M
+```
+
+### 2. Large Model
+```text
+File Name: 
+Description: BERT-Large, Uncased
+Language Type: 英文
+Network Structure: 24-layer, 1024-hidden, 16-heads
+Parameter Scale: 340M
+```
---
+++ utils/dataset.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+
+class DataSet:
+
+    def __init__(self):
+        self.dataset_path = "../data/{}.txt"
+        self.dataset = {"label": [], "text": []}
+        self.POSITIVE_LABEL, self.NEGATIVE_LABEL = 0, 1
+
+    def get_data(self, dataset_type):
+        # Train dataset size: 2420, Valid dataset size: 632, Test dataset size: 888
+        dataset = pd.read_table(self.dataset_path.format(dataset_type), sep="\t")
+        texts = []
+        labels = []
+        for item in dataset.itertuples():
+            positive_text = "{} {} {} And since {}, {}.".format(
+                item.debateTitle, item.debateInfo, item.reason, item.warrant0, item.claim) \
+                if item.correctLabelW0orW1 == 0 else "{} {} {} And since {}, {}.". \
+                format(item.debateTitle, item.debateInfo, item.reason, item.warrant1, item.claim)
+            negative_text = "{} {} {} And since {}, {}.".format(
+                item.debateTitle, item.debateInfo, item.reason, item.warrant1, item.claim) \
+                if item.correctLabelW0orW1 == 0 else "{} {} {} And since {}, {}.". \
+                format(item.debateTitle, item.debateInfo, item.reason, item.warrant0, item.claim)
+            texts.append(positive_text)
+            labels.append(0)
+            texts.append(negative_text)
+            labels.append(1)
+        self.dataset["label"] = labels
+        self.dataset["text"] = texts
+        return self.dataset
+
+
+if __name__ == '__main__':
+    train_data = DataSet().get_data(dataset_type="train")
+    valid_data = DataSet().get_data(dataset_type="valid")
+    test_data = DataSet().get_data(dataset_type="test")
+    print(pd.DataFrame(train_data))
